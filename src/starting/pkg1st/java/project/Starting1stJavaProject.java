c325532ae5ae2c4a1d1eb78d2bdd29086f68e775
class <class@CST_N417_02.cst.citytech.cuny.edu>
2015-02-11 16:54:04 -0500

    Initial commit for chapter 2

---
+++ src/starting/pkg1st/java/project/Starting1stJavaProject.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package starting.pkg1st.java.project;
+
+/**
+ *
+ * @author class
+ */
+public class Starting1stJavaProject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        system.out;println("*");
+    }
+    
+}
